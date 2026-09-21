ad87f6bf7294f9efe113a3fbf2ae30fbaffda70e
shiv <blue.4209211@gmail.com>
2026-09-21 10:01:25 +0530

    fix(migrations): reconcile EE 1.9 migration history

--- api-server/migrations/migrations/app/1786997559724_V890_openobserve_webhook_integration.down.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM event_rule_source WHERE value = 'openobserve_webhook';
-
-DELETE FROM event_source WHERE value = 'openobserve_webhook';
-
-DELETE FROM "public"."integration_types" WHERE "name" = 'openobserve_webhook';
